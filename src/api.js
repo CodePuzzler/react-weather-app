c0bafef846a5d964dff994446f350a7fba1b9fcf
Amit <ay.puzzler@gmail.com>
2022-08-29 12:28:16 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'a278967af8msh203c73a92c71749p1d7875jsn676209f03ce8',
+    'X-RapidAPI-Key': '', //YOUR-OWN-API-KEY
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 }
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = '5cfbdc1e0853efcf998b619c0e68faea'
+export const WEATHER_API_KEY = '' //YOUR-OWN-API-KEY
