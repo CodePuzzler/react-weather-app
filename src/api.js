4888594f26b5f8c4efff22a942eda4975512a744
Amit Yadav <ay.puzzler@gmail.com>
2022-08-29 12:25:37 +0530

    app completed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'a278967af8msh203c73a92c71749p1d7875jsn676209f03ce8',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '5cfbdc1e0853efcf998b619c0e68faea'
